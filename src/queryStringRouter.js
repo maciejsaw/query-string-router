5f2452fbaa6953d7bf696ccd1896ee3c2679c8e4
Maciej Sawicki <saw.maciej@gmail.com>
2018-04-25 16:43:32 +0200

    Removed Meteor dependency

--- src/queryStringRouter.js
+++ src/queryStringRouter.js
@@ -1,8 +1,6 @@
+//QueryStringRouter - designed by Maciej Sawicki documented on https://github.com/maciejsaw/query-string-router
+
 var QueryStringRouter = (function() {
-
-	/*----------  Reacting to route changes  ----------*/
-
-	var reactiveRouterParams = new ReactiveMap();
 
 	//decode query string
 	function getQueryStringParams() {
@@ -13,79 +11,71 @@ var QueryStringRouter = (function() {
 		return deparam(window.location.search.substring(1))[key];
 	} 
 
-	//each time this function is fired, all Meteor.run functions will run, but only
-	//the ones for which the value changed
-
 	//we diff against the previous params, so that we can remove the params that are not present
 	//in the query string from the reactive params
 	var previousQueryStringParams = {};
 	function processQueryStringParams() {
 		var queryStringParams = getQueryStringParams();
-		//console.log(previousQueryStringParams);
 
 		//check what previous params are not present in the new query string
 		$.each(previousQueryStringParams, function(key, value) {
 			if (typeof queryStringParams[key] == 'undefined') {
-				reactiveRouterParams.set(key, undefined);
+				$(document).trigger('QueryStringRouter__'+key+'__paramChanged');
+				console.log('param '+key+' was removed');
 			}
 		});
 
 		$.each(queryStringParams, function(key, value) {
-			reactiveRouterParams.set(key, value);
+			if (queryStringParams[key] !== previousQueryStringParams[key]) { //only trigger event if param changed
+				$(document).trigger('QueryStringRouter__'+key+'__paramChanged');
+			}
 		});
 
-		previousQueryStringParams = queryStringParams
-
-		//console.log(queryStringParams);
-		//console.log(reactiveRouterParams);
-
-		return queryStringParams;	
+		previousQueryStringParams = queryStringParams;
 	}
 
-	//when document loads, set reactive params 
+	//when document loads
 	processQueryStringParams();
 
-	//when url changes, also set reactive params
 	$(window).on('popstate', function() {
 		processQueryStringParams();
-		//console.log('popstate');
 	});
 
-	//when ajax reloads something, also trigger the reactive functions.
-	//They should not do anything if nothing was changed
-	$(document).on('ajaxComplete', function() {
-		//retrigger actions for all params, so that the ajax loaded views are updated
-		//TODO: how to do this?
-	});
+	function setParam(key, value, options) {
+		var queryStringParams = getQueryStringParams();
 
-	function queryStringRouterSetParam(key, value, options) {
-		var queryStringParams = deparam(window.location.search.substring(1));
-		queryStringParams[key] = value;
-		var newQueryString = $.param(queryStringParams);
+		if (queryStringParams[key] !== value) {
+			queryStringParams[key] = value;
+			var newQueryString = $.param(queryStringParams);
 
-		options = options || {};
-		if (options.doNotCreateHistoryState === true) {
-			window.history.replaceState('','', '?'+newQueryString);
-		} else {
-			window.history.pushState('','', '?'+newQueryString);
+			options = options || {};
+			if (options.doNotCreateHistoryState === true) {
+				window.history.replaceState('','', '?'+newQueryString);
+			} else {
+				window.history.pushState('','', '?'+newQueryString);
+			}
+
+			$(window).trigger('popstate');
 		}
-
-		$(window).trigger('popstate');    
+		
 	}
 
 	function removeParam(key, options) {
-		var queryStringParams = deparam(window.location.search.substring(1));
-		delete queryStringParams[key];
-		var newQueryString = $.param(queryStringParams);
+		var queryStringParams = getQueryStringParams();
+		if (typeof queryStringParams[key] !== 'undefined') {
+			delete queryStringParams[key];
 
-		options = options || {};
-		if (options.doNotCreateHistoryState === true) {
-			window.history.replaceState('','', '?'+newQueryString);
-		} else {
-			window.history.pushState('','', '?'+newQueryString);
+			var newQueryString = $.param(queryStringParams);
+
+			options = options || {};
+			if (options.doNotCreateHistoryState === true) {
+				window.history.replaceState('','', '?'+newQueryString);
+			} else {
+				window.history.pushState('','', '?'+newQueryString);
+			}
+
+			$(window).trigger('popstate');  
 		}
-
-		$(window).trigger('popstate'); 
 	}
 
 	function setFreshParams(newParamsObj, options) {
@@ -97,20 +87,15 @@ var QueryStringRouter = (function() {
 		} else {
 			window.history.pushState('','', '?'+newQueryString);
 		}
-		$(window).trigger('popstate'); 
-	}
-
-	function queryStringRouterGetReactiveParam(key) {
-		return reactiveRouterParams.get(key);
+		$(window).trigger('popstate');
 	}
 
 	var actionsOnParamChange = {};
 	function onParamChange(key, actionFunction) {
-		//for each key in the reactive router params, define a reactive autorun 
-		//Each of the params will react to change independently, only if changed
-		Meteor.run(function() {
-			var param = reactiveRouterParams.get(key); //note that this is reactive getParam, not the static getQueryStringParam used elsewhere
-			actionFunction(param);
+		$(document).on('QueryStringRouter__'+key+'__paramChanged', function(event) {
+			var paramsObject = getQueryStringParams();
+			var value = paramsObject[key];
+			actionFunction(value);
 		});
 
 		//store the action on param in a separate array, so that we can retrigger this route manually
@@ -119,6 +104,9 @@ var QueryStringRouter = (function() {
 			actionsOnParamChange[key] = [];
 		}
 		actionsOnParamChange[key].push(actionFunction);
+
+		//when the onParamChanged is being defined, also retrigger the state
+		retriggerOnParamChange(key);
 	}
 
 	function retriggerOnParamChange(key) {
@@ -144,17 +132,20 @@ var QueryStringRouter = (function() {
 	}
 
 	return {
-		setParam: queryStringRouterSetParam,
-		getAllParams: processQueryStringParams,
+		setParam: setParam,
+		getParam: getQueryStringParam,
+		getAllParams: getQueryStringParams,
 		setFreshParams: setFreshParams,
 		setDefaultRootParams: setDefaultRootParams,
-		getParam: queryStringRouterGetReactiveParam,
 		onParamChange: onParamChange,
 		retriggerOnParamChange: retriggerOnParamChange,
 		retriggerOnParamChangeForAll: retriggerOnParamChangeForAll,
 		removeParam: removeParam,
-		version: '1.0'
-	}
+		version: '21',
+		releaseNotes: {
+			v21: 'only fire events when param changed',
+			v2: 'removed Meteor and uses simple jQuery events',
+		}
+	};
 
 })();
-
